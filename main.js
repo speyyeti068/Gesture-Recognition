cc7dead6f27db3cb166f1f94cc2dad1bc77e088f
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-04-29 20:41:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+
+rightWristX = 0;
+leftWristX = 0;
+difference = 100;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(500,400);
@@ -12,12 +19,29 @@ function setup(){
     poseNet.on("pose",function(results){
         console.log("Got Results");
 
-        if (results.length >= 0){
+        if (results.length > 0){
             console.log(results);
+
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("Nose X = " + noseX, "Nose Y = " + noseY);
+
+            rightWristX = results[0].pose.rightWrist.x;
+            leftWristX = results[0].pose.leftWrist.x;
+            console.log("Right Wrist X = " + rightWristX, "Left Wrist X = " + leftWristX);
+
+            difference = floor(leftWristX - rightWristX);
+            console.log("Difference = " + difference);
         }
     })
 }
 
 function draw(){
     background("red");
+
+    fill("aqua");
+    stroke("yellow");
+    square(noseX,noseY,difference);
+
+    document.getElementById("squareSize").innerHTML = "The Size Of The Square Is Aproximatly " + difference + " px";
 }
